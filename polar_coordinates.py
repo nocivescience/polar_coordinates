b6609bc2e9eef448ad9e4ba931bdc71755776134
noceive <nocivescience@gmail.com>
2022-05-18 20:24:57 -0400

    terminando show_xy_axis

--- polar_coordinates.py
+++ polar_coordinates.py
@@ -1,2 +1,15 @@
 from manim import *
-class 
+class PolarCoordinatesScene(MovingCameraScene):
+    CONFIG={
+        'x_color':YELLOW,
+        'y_color':RED,
+        'r_color':GREEN,
+        'theta_color':BLUE,
+    }
+    def construct(self):
+        self.show_xy_axis()
+    def show_xy_axis(self):
+        plane=NumberLine()
+        plane.add_coordinates()
+        self.play(Create(plane))
+        self.wait()
