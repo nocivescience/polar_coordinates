1dde03b53a5e17511b2d8be4c48544d2342f3004
noceive <nocivescience@gmail.com>
2022-05-19 01:08:46 -0400

    proyectos con los parantesis de llave pero fueron retirados

--- polar_coordinates.py
+++ polar_coordinates.py
@@ -17,23 +17,36 @@ class PolarCoordinatesScene(MovingCameraScene):
         yp=plane.c2p(0,y)
         origin=plane.c2p(0,0)
         point=plane.c2p(x,y)
-        x_line=Line(point,xp)
-        y_line=Line(point,yp)
         dot=Dot(point,color=self.CONFIG['x_color'])
+        x_line=Line(point,xp).add_updater(lambda m:m.put_start_and_end_on(dot.get_center(),-dot.get_center()[1]*DOWN))
+        y_line=Line(point,yp).add_updater(lambda m:m.put_start_and_end_on(dot.get_center(),-dot.get_center()[0]*LEFT))
         #get_coord_label
         coord_label=self.get_coord_label(x,y,self.CONFIG['x_color'],self.CONFIG['y_color'])
         coord_label.next_to(dot,UR,buff=SMALL_BUFF)
-        x_brace=Brace(x_line,UP)
-        y_brace=Brace(y_line,RIGHT)
+        x_brace=Brace(x_line,RIGHT).add_updater(lambda m:m.next_to(y_line,RIGHT,buff=SMALL_BUFF))
+        y_brace=Brace(y_line,UP).add_updater(lambda m:m.next_to(x_line,UP,buff=SMALL_BUFF))
         plane.add_coordinates()
         for mob in [
-            plane,dot,
+            plane,
+            dot,
             x_line,
             y_line,
             x_brace,
             y_brace
         ]:
             self.play(Create(mob))
+        self.add(x_brace,y_brace)
+        self.wait()
+        for mob in [
+            LEFT+.1*UP,
+            RIGHT+.1*UP,
+            LEFT+.4*DOWN,
+            RIGHT+.4*DOWN,
+        ]:
+            self.play(
+                dot.animate.shift(mob)
+            )
+        self.play(dot.animate.move_to(point))
         self.wait()
     def get_coord_label(self,x=0,y=0,x_color=WHITE,y_color=WHITE,include_background_rectangle=True,**decimal_kwargs):
         coords=VGroup()
