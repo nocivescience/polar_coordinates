f0b837885d1fa02f32de80d313a07301f0e99f34
noceive <nocivescience@gmail.com>
2022-05-20 01:23:03 -0400

    escena que incluye el radio rotando

--- polar_coordinates.py
+++ polar_coordinates.py
@@ -17,13 +17,29 @@ class PolarCoordinatesScene(MovingCameraScene):
         yp=plane.c2p(0,y)
         origin=plane.c2p(0,0)
         point=plane.c2p(x,y)
-        x_line=Line(point,xp)
-        y_line=Line(point,yp)
-        x_brace=Brace()
+        x_line=Line(point,xp).add_updater(lambda m: m.put_start_and_end_on(dot.get_center(),np.array([dot.get_center()[0],0,0])))
+        y_line=Line(point,yp).add_updater(lambda m: m.put_start_and_end_on(dot.get_center(),np.array([0,dot.get_center()[1],0])))
+        x_brace=always_redraw(lambda:Brace(y_line,UP,buff=SMALL_BUFF))
+        y_brace=always_redraw(lambda:Brace(x_line,RIGHT,buff=SMALL_BUFF))
         dot=Dot(point,color=self.CONFIG['x_color'])
+        x_coord=MathTex('x',color=self.CONFIG['x_color']).add_updater(lambda m: m.next_to(x_brace,UP,buff=SMALL_BUFF))
+        y_coord=MathTex('y',color=self.CONFIG['y_color']).add_updater(lambda m: m.next_to(y_brace,RIGHT,buff=SMALL_BUFF))
         plane.add_coordinates()
-        for mob in [plane,dot,x_line,y_line]:
+        for mob in [plane,dot,x_line,y_line,x_brace,y_brace,x_coord,y_coord]:
             self.play(Create(mob))
+        self.wait()
+        for pos in [LEFT+0.5*UP,LEFT+0.5*DOWN,RIGHT+0.5*DOWN,RIGHT+0.5*UP]:
+            self.play(dot.animate.shift(pos))
+        self.play(dot.animate.move_to(point))
+        axes_polar,circles,rays=self.get_polar_grid(plane)
+        self.play(ReplacementTransform(plane,VGroup(axes_polar,circles,rays)))
+        self.wait()
+        polar_coord,radius_line,arco=self.get_polar_coord(dot,origin)
+        self.play(ReplacementTransform(VGroup(x_coord,y_coord),polar_coord))
+        self.wait()
+        self.play(ReplacementTransform(VGroup(x_line,y_line,x_brace,y_brace),radius_line))
+        self.play(TransformFromCopy(polar_coord[-2],arco))
+        self.play(Rotating(radius_line,radians=np.pi*2,about_point=origin))
         self.wait()
     def get_coord_label(self,x=0,y=0,x_color=WHITE,y_color=WHITE,include_background_rectangle=True,**decimal_kwargs):
         coords=VGroup()
@@ -52,22 +68,30 @@ class PolarCoordinatesScene(MovingCameraScene):
         if include_background_rectangle:
             coord_label.add_background_rectangle()
         return coord_label
-    def show_polar_coordinates(self,dot,plane):
-        origin=plane.c2p(0,0)
-        r_line=Line(origin,dot.get_center())
-        r_line.set_color(self.CONFIG['r_color'])
-        r_value=r_line.get_length()
-        theta_value=r_line.get_angle()
-        coord_label=self.get_coord_label(r_value,theta_value,self.CONFIG['r_color'],self.CONFIG['theta_color'])
-        r_coord=coord_label.x_coord
-        theta_coord=coord_label.y_coord
-        coord_label.add_updater(lambda m: m.next_to(dot,RIGHT,buff=0))
-        r_coord.add_updater(lambda m: m.set_value(r_line.get_length()))
-        theta_value.add_updater(lambda m: m.set_value(r_line.get_angle()))
-        self.play(Create(coord_label))
     def transition_to_polar_coord(self):
         pass
     def get_polar_grid(self,plane,radius=0.25):
-        axes=VGroup(
-            
-        )
+        circles=VGroup(*[
+            Circle(color=BLUE,stroke_width=1,radius=r)
+            for r in np.arange(0.5,8.5,0.5)
+        ])
+        rays=VGroup(*[
+            Line(plane.c2p(0,0),RIGHT*8.5,stroke_width=1,color=BLUE).set_angle(r)
+            for r in np.arange(0,2*np.pi,np.pi/12)
+        ])
+        return VGroup(circles,rays)
+    def get_polar_coord(self,dot,origin):
+        theta_coord=MathTex("\\theta",color=self.CONFIG['theta_color'])
+        r_coord=MathTex("r",color=self.CONFIG['r_color'])
+        set_coord=VGroup(
+            MathTex("("),
+            r_coord,
+            MathTex(","),
+            theta_coord,
+            MathTex(")"),
+        ).arrange(RIGHT,buff=SMALL_BUFF)
+        set_coord[2].align_to(set_coord[0],DOWN)
+        set_coord.add_updater(lambda m: m.next_to(dot,UP,buff=SMALL_BUFF))
+        radius=Line(origin,dot.get_center())
+        arco=Angle(Line(origin,RIGHT),radius,radius=1,color=WHITE)
+        return [set_coord,radius,arco]
